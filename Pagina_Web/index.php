58d5bbbc0bc5d31a47f75a888a45f21203b6a0b7
NelsonM18 <66973737+NelsonM18@users.noreply.github.com>
2020-07-09 10:03:04 -0500

    Form Inicio de Sesion
    Docs: Se agrego un form el cual contiene el inicio de sesion y se le agrego sus respectivos estilos.

--- Pagina_Web/index.php
+++ Pagina_Web/index.php
@@ -1,1 +1,12 @@
 <h1>Holaaa</h1>
+
+<form class="" action="" method="post">
+
+    <h2>Iniciar Sesion</h2><br>
+    <label>Correo</label><br>
+    <input type="email" name="" value=""><br>
+    <label>Contraseña</label><br>
+    <input type="password" name="" value=""><br><br>
+    <input type="submit" name="" value="Iniciar Sesion">
+
+</form>
